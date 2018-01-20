f8b143183412dd5b5d7f0977aaeeba4a8fbf22cf
Oran C <oranbusiness@gmail.com>
2018-01-20 14:45:07 -0800

    created node js  setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HDProj
+Hack davis project for metal health 
+180120
+node server for getting and managing sentiment anallasis
+of facebook posts
+
+tech used nodejs
---
+++ server.js
@@ -0,0 +1,10 @@
+//Hack davis project for metal health 
+//180120
+//node server for getting and managing sentiment anallasis
+//of facebook posts
+
+//tech used nodejs
+
+
+
+print("Hack davis health app server has started");
