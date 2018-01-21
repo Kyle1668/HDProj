2607884e88ac026fe404d906a35190558ad761bc
Oran C <oranbusiness@gmail.com>
2018-01-20 18:29:13 -0800

    cleaned up dir's, added sentiment class starting file

--- server.js
+++ server.js
@@ -6,16 +6,33 @@
 //tech used nodejs
 
 require("wise-helper");
+// https://www.npmjs.com/package/request
 var request = require('request');
+// https://www.npmjs.com/package/query-string
+var queryString = require('query-string');
 
+var data = {};
+
+data.input =  "The Last Jedi was the best Star Wars movie ever!";
 
 console.log("Hack davis health app server has started");
 
 
 
 
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+
+var stringified = queryString.stringify(data);
+var API_URL = `http://lisent.herokuapp.com/get?${stringified}`;
+
+console.log(stringified);
+console.log(API_URL);
+
+request(API_URL, function (error, response, body) {
+//todo add error handeling
+	var json_returned = JSON.parse(body);
+	console.log("Sentament anallasis of setense");
+	console.log(`Positive :${json_returned.ppos}%`);
+	console.log(`Negative :${json_returned.pneg}%`);
+	// console.log(json_returned.pneg);	
+  // console.log(body); // Print the HTML for the Google homepage.
 });
