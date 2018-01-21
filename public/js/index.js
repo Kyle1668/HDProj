eb4ce510aed80157c2de722f567591a565670cab
AndrewE1234 <andrew@omnicode.com>
2018-01-21 09:53:08 -0800

    Added data box

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,16 @@
 var someData = 0;
 
+var url = "";
+var bigData ="test";
 $(".btn-default").click(function ()
 {
 	console.log("Test!");
 	$(".site-wrapper-inner").empty();
-	addToList("TESTT");
+	addToList("TEST");
 })
 
-function addToList(bigData)
+function addToList()
 {
-	$(".site-wrapper-inner").append(`<p class="col-center">${bigData}</p>`);
+	$(".site-wrapper-inner").append(`<div class = "data-box"><p>${bigData}</p></div>`);
+
 }
