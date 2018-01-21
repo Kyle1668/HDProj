7c59f4ae0585787f4cac4520d07d18a0cba2e6b6
AndrewE1234 <andrew@omnicode.com>
2018-01-20 20:52:11 -0800

    Started on adding data to new screen

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,13 @@
+var someData = 0;
+
 $(".btn-default").click(function ()
 {
 	console.log("Test!");
 	$(".site-wrapper-inner").empty();
+	addToList("TESTT");
 })
+
+function addToList(bigData)
+{
+	$(".site-wrapper-inner").append(`<p class="col-center">${bigData}</p>`);
+}
